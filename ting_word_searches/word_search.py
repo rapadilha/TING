c9431ff0d243817619f7bbfb9d5f614efce71544
Raphael <raphaellfpg@gmail.com>
2022-12-13 03:31:19 -0300

    feat: Adiciona a funcao exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    finds = list()
+    lines = list()
+    for item in range(len(instance)):
+        count = 0
+        test = instance.search(item)
+        for line in test['linhas_do_arquivo']:
+            count += 1
+            if word.lower() in line.lower():
+                lines.append({'linha': count})
+    model ={
+            "palavra": word,
+            "arquivo": test['nome_do_arquivo'],
+            "ocorrencias": lines
+        }
+    if lines == []:
+        return []
+    finds.append(model)
+    return finds
+
 
 
 def search_by_word(word, instance):
