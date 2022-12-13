f20898b81629c6a321cdf9b6dc2b2f38fd9ec688
Raphael <raphaellfpg@gmail.com>
2022-12-13 01:50:58 -0300

    feat: Adiciona a funcao txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith('.txt'):
+            result = list()
+            with open(path_file, 'r') as file:
+                for line in file:
+                    result.append(line.rstrip())
+            return result
+        else:
+            print(sys.stderr.write('Formato inválido\n'))
+    except FileNotFoundError:
+        print(sys.stderr.write(f'Arquivo {path_file} não encontrado\n'))
