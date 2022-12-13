5c544e1697ef8f5ef7b3321d91078ec767dcd281
Raphael <raphaellfpg@gmail.com>
2022-12-13 02:24:39 -0300

    feat: Adiciona a funcao process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management import file_management
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = file_management.txt_importer(path_file)
+    model = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+
+    for path in range(len(instance)):
+        if path_file == instance.search(path)["nome_do_arquivo"]:
+            return None
+    instance.enqueue(model)
+    print(model)
 
 
 def remove(instance):
